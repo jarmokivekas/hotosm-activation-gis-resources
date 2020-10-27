43b39c0d504c96395d0af661a874b594fd336496
Jarmo Kivekäs <jarmo.kivekas@gmail.com>
2020-10-27 21:08:46 +0200

    projects-by-organization: add better logging; support for project statuses

--- notebooks/project-geometries-by-organisation-id/project-geometries-by-organisation-id.py
+++ notebooks/project-geometries-by-organisation-id/project-geometries-by-organisation-id.py
@@ -1,4 +1,33 @@
-from IPython.display import JSON
+"""
+Zero-Clause BSD
+
+Permission to use, copy, modify, and/or distribute this software for any
+urpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+"""
+
+"""
+Usage:
+
+Configure of the search parameters below (INSTANCE and ORG_ID)
+and call this script without any arguments. output will be written to ./output
+relative to your working directory (the directory is created if missing).
+Existing files are over written.
+"""
+
+# this is the default HOTOSM tasking manager instance
+INSTANCE = 'https://tasking-manager-tm4-production-api.hotosm.org'
+# The Tasking Manager organisationId we are listing projects for
+ORG_ID = 34
+
+
 import json
 import requests
 import pandas as pd
@@ -6,27 +35,21 @@ import logging
 import pathlib
 
 
-# Confiugration of the search.
-# this is the default HOTOSM tasking manager instance
-INSTANCE = 'https://tasking-manager-tm4-production-api.hotosm.org'
-# The Tasking Manager organisationId we are listing projects for
-orgId = 34
-
-
-
-
 def main():
-
-
     # for collecting rearch results
     results = []
     page = 1
 
-    logging.INFO('start fetching search result pages')
+    logging.info('start fetching search result pages')
     while 1:
-        url = f'{INSTANCE}/api/v2/projects/?organisationId={orgId}&page={page}'
-        logging.debug(f'fetching: {url}')
-        resp = requests.get(url)
+        url = f'{INSTANCE}/api/v2/projects/'
+        params = {
+            'organisationId': ORG_ID,
+            'page': page,
+            'projectStatuses': "ARCHIVED,PUBLISHED,DRAFT"
+        }
+        logging.debug(f'fetching: {url}, {params}')
+        resp = requests.get(url, params=params)
         resp = json.loads(resp.content)
 
         results += resp['results']
@@ -38,12 +61,12 @@ def main():
             page = resp['pagination']['nextNum']
         else:
             # all pages have been appended to `results`
-            logging.INFO(f'projects returned by search: {len(results)}')
+            logging.info(f'projects returned by search: {len(results)}')
             break
 
     df = pd.DataFrame(results)
-    metadatafile = f'output/tm-projects-organisationId-{orgId}.csv'
-    logging.debug(f'using metadata output file: {metadatafile})
+    metadatafile = f'output/tm-projects-organisationId-{ORG_ID}.csv'
+    logging.debug(f'using metadata output file: {metadatafile}')
     df.to_csv(metadatafile)
 
 
